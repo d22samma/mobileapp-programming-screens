f0bbc4a020d20603cd45f453c88ac444042a1e8e
d22samma <d22samma@student.his.se>
2023-04-18 11:21:54 +0200

    Added Intent

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.screens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
 import android.os.Bundle;
@@ -18,7 +19,13 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, second_activity.class);
+                intent.putExtra("food", "Potato"); // Optional
+                intent.putExtra("age", 1); // Optional
+                startActivity(intent);
+
                 setContentView(R.layout.activity_second);
+
             }
         });
     }
--- app/src/main/java/com/example/screens/second_activity.java
+++ app/src/main/java/com/example/screens/second_activity.java
@@ -10,5 +10,12 @@ public class second_activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            String name = extras.getString("food");
+            int number = extras.getInt("age");
+            // Do something with the name and number
+        }
     }
 }
