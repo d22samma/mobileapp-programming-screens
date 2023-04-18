1ad4d4ef88f072bf2a2e1e205b2e87b5bd6e6ba7
d22samma <d22samma@student.his.se>
2023-04-18 11:54:20 +0200

    Added a Backward button on second_activity.java

--- app/src/main/java/com/example/screens/second_activity.java
+++ app/src/main/java/com/example/screens/second_activity.java
@@ -2,20 +2,32 @@ package com.example.screens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class second_activity extends AppCompatActivity {
+    private Button btnClick;
+
+    Bundle extras = getIntent().getExtras();
+    if (extras != null) {
+        String name = extras.getString("name");
+        int number = extras.getInt("number");
+        // Do something with the name and number
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-
-        Bundle extras = getIntent().getExtras();
-        if (extras != null) {
-            String name = extras.getString("food");
-            int number = extras.getInt("age");
-
-        }
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                Intent intent = new Intent(second_activity.this, MainActivity.class);
+                startActivity(intent);
+            }
+            });
     }
 }
